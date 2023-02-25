c043d74cb860889dfec25d630b61a933cb1f0ee9
Ben Wilson <doubtingben@gmail.com>
2023-02-24 20:06:42 -0500

    Add TF secret to atlantis deployment

--- atlantis/terraform/main.tf
+++ atlantis/terraform/main.tf
@@ -19,19 +19,19 @@ resource "github_repository_webhook" "webhook" {
   repository = data.github_repository.repo.name
 
   configuration {
-    url          = var.atlantis-webhook-url
+    url          = var.atlantis_webhook_url
     content_type = "application/json"
-    secret       = var.atlantis-webhook-secret
+    secret       = var.atlantis_webhook_secret
   }
   events = ["pull_request", "pull_request_review_comment", "pull_request_review", "issue_comment", "push"]
 }
 
-variable "atlantis-webhook-url" {
+variable "atlantis_webhook_url" {
   type        = string
   description = "The URL of the Atlantis webhook"
   default     = "https://atlantis-2023-02-24.yooser.xyz/events"
 }
-variable "atlantis-webhook-secret" {
+variable "atlantis_webhook_secret" {
   type        = string
   sensitive   = true
   description = "The secret of the Atlantis webhook"
